3e23515c8e072b090ec71c6932744c1d63af2b0f
Fabrício Felizardo <fabricioemanuel20@gmail.com>
2023-08-16 21:27:27 -0300

    main

---
+++ rpg-wiki/src/main/java/com/tabletale/rpgwiki/RpgWikiApplication.java
@@ -0,0 +1,13 @@
+package com.tabletale.rpgwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgWikiApplication.class, args);
+	}
+
+}
---
+++ rpg-wiki/src/test/java/com/tabletale/rpgwiki/RpgWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabletale.rpgwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
